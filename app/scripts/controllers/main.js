44fd13884da12d2b3da1987777d6b0862b2c9551
Markus Török <markus.toeroek@gmail.com>
2014-02-07 13:22:59 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('connectfourApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
