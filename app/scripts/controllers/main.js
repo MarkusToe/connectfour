ccc66fd9b5d0b5f7939fe384a9d4d97db4cc366a
Markus Török <markus.toeroek@gmail.com>
2014-02-09 14:52:51 +0100

    only last token is added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,7 @@
 app.controller('boardCtrl', function ($scope) {
     $scope.fields = [];
     $scope.boardRows = [];
+    $scope.boardCols = [];
 
     $scope.columns = 8;
     $scope.rows = 8;
@@ -19,23 +20,39 @@ app.controller('boardCtrl', function ($scope) {
     // push fields in array for each column
     for(var i=0; i < $scope.columns; i++){
         $scope.boardRows.push([]);
+        $scope.boardCols.push([]);
         for(var j= 0; j < $scope.fields.length; j++){
             if($scope.fields[j].x == i+1){
                 $scope.boardRows[$scope.boardRows.length-1].push($scope.fields[j]);
+            }
+            if($scope.fields[j].y == i+1){
+                $scope.boardCols[$scope.boardCols.length-1].push($scope.fields[j]);
             }
         }
     }
 
     $scope.setToken = function(selectedField){
-        selectedField.occupied = true;
 
-        // switch player on each click
-        if($scope.playerOneTurn){
-            selectedField.color = "player-one";
-            $scope.playerOneTurn = !$scope.playerOneTurn;
-        } else {
-            selectedField.color = "player-two";
-            $scope.playerOneTurn = !$scope.playerOneTurn;
+        var colNumber = selectedField.y - 1;
+        var currentCol = $scope.boardCols[colNumber];
+
+        for(var i=currentCol.length; i>=0; i--){
+
+            if(currentCol[i-1].occupied == false){
+                var topToken = $scope.boardCols[colNumber][i-1];
+
+                topToken.occupied = true;
+
+                // switch player on each click
+                if($scope.playerOneTurn){
+                    topToken.color = "player-one";
+                    $scope.playerOneTurn = !$scope.playerOneTurn;
+                } else {
+                    topToken.color = "player-two";
+                    $scope.playerOneTurn = !$scope.playerOneTurn;
+                }
+                return;
+            }
         }
     }
 });
