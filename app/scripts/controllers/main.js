ddd667296f3a4a648be9ed9b6e5352accc1f3e43
Markus Török <markus.toeroek@gmail.com>
2014-02-10 14:14:07 +0100

    game logic added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,7 @@ app.controller('boardCtrl', function ($scope) {
     $scope.columns = 8;
     $scope.rows = 8;
     $scope.playerOneTurn = true;
+    $scope.gameOver = false;
 
     //    create fields
     for(var i=1; i<=$scope.columns; i++ ){
@@ -31,8 +32,8 @@ app.controller('boardCtrl', function ($scope) {
         }
     }
 
+    // put tokens on board
     $scope.setToken = function(selectedField){
-
         var colNumber = selectedField.y - 1;
         var currentCol = $scope.boardCols[colNumber];
 
@@ -51,10 +52,129 @@ app.controller('boardCtrl', function ($scope) {
                     topToken.color = "player-two";
                     $scope.playerOneTurn = !$scope.playerOneTurn;
                 }
+                $scope.fourConnected(topToken, $scope.boardRows, $scope.boardCols, $scope.rows, $scope.columns);
                 return;
             }
         }
     }
+
+    // check if player has four in a row
+    $scope.fourConnected = function(field, rows, columns, rowCount, colCount){
+        var i, c, r, counter;
+
+        var x = field.x;
+        var y = field.y;
+
+        var checkColor = field.color;
+
+        function alreadyFour(c) {
+            if(c >= 4) {
+                // document.getElementById('infotext').innerHTML = '&nbsp;&nbsp;hat gewonnen';
+                console.log("game Over");
+                $scope.gameOver = true;
+            }
+        }
+
+        //horizontal -----------------------
+        counter = 0;
+        for(i = 0; i < colCount; i++) {
+
+            if(rows[x-1][i].color === checkColor) {
+                counter++;
+                //console.log('horizontal: ' + counter);
+            }
+            else {
+                counter = 0;
+            }
+            alreadyFour(counter);
+        }
+
+        //vertical -----------------------
+        counter = 0;
+        for(i = x; i <= 8 ; i++) {
+            if(rows[i-1][y-1].color === checkColor)
+            {
+                counter++;
+                // console.log('vertical: ' + counter);
+            }
+            else {
+                counter = 0;
+            }
+            alreadyFour(counter);
+        }
+
+        //diagonal right-down -----------------------
+        counter = 0;
+        r = x;
+        c = y;
+
+        // get to upper left corner (as far as possible)
+        while(r > 0 && c > 0) {
+            r--;
+            c--;
+        }
+
+        for(i = 0; i < colCount; i++) {
+
+            if(rows[r] && c<colCount) {
+
+                //console.table(rows[r]);
+                //console.log("column at the moment: " + c + " | row at the moment: " + r);
+
+                if(rows[r][c].color === checkColor) {
+                    counter++;
+                    // console.log('diagonal right down: ' + counter);
+                }
+                else {
+                    counter = 0;
+                }
+                alreadyFour(counter);
+                r++;
+                c++;
+
+            }
+            else {
+                break;
+            }
+        }
+
+        //diagonal left-down -----------------------
+        counter = 0;
+        r = x;
+        c = y;
+c
+        // get up to right corner (as far as possible)
+        while(r > 0 && c < rowCount) {
+            r--;
+            c++;
+        }
+
+        for(i = 0; i < colCount; i++) {
+            if(rows[r-1] && c >= 0) {
+                if(rows[r-1][c-1].color == "player-one") {
+                    console.log("current row: " + r);
+                    console.log("current column: " + c);
+                }
+                if(rows[r-1][c-1].color === checkColor) {
+                    counter++;
+                    if(rows[r-1][c-1].color == "player-one") {
+                        console.log('diagonal left down' + counter + "\n");
+                    }
+                }
+                else {
+                    counter = 0;
+                }
+                alreadyFour(counter);
+
+                r++;
+                c--;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
 });
 
 app.directive('field', function (){
